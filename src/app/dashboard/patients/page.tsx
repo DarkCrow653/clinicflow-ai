c1040d43622a768d9e754e4c9b1e322071c0ca2d
DarkCrow653 <DarkCrow653@email.com>
2026-06-09 20:57:36 -0700

    test deployment

--- src/app/dashboard/patients/page.tsx
+++ src/app/dashboard/patients/page.tsx
@@ -74,6 +74,11 @@ export default function PatientsPage() {
 
   return (
     <div className="space-y-8 p-10">
+       
+      <h1 className="text-red-500 text-4xl">
+        VERSION NUEVA
+      </h1>
+
       <div>
         <h1 className="text-4xl font-bold">
           Pacientes
